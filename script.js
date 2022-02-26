5d4eabc3a9e32ed568fd8d5905eea937bb01f20f
Zurishaddai Temitope <temitopezurishaddai@gmail.com>
2022-02-26 19:27:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const formOne = document.getElementById('form-one');
+const formTwo = document.getElementById('form-two');
+const formThree = document.getElementById('form-three');
+
+const nextOne = document.getElementById('next-one');
+const nextTwo = document.getElementById('next-two');
+const backOne = document.getElementById('back-one');
+const backTwo = document.getElementById('back-two');
+const progress = document.getElementById('progress');
+
+nextOne.onclick = function(){
+    formOne.style.left = '-450px';
+    formTwo.style.left = '40px';
+    progress.style.width = '240px'
+}
+backOne.onclick = function(){
+    formOne.style.left = '40px';
+    formTwo.style.left = '450px';
+    progress.style.width = '140px'
+}
+nextTwo.onclick = function(){
+    formTwo.style.left = '-450px';
+    formThree.style.left = '40px';
+    progress.style.width = '360px'
+}
+backTwo.onclick = function(){
+    formTwo.style.left = '40px';
+    formThree.style.left = '450px';
+    progress.style.width = '240px'
+}
